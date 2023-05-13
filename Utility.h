b20606ce8c1c06d3ab96c8218a49f9e72b9786e1
fauder_home <canik.burak@gmail.com>
2023-05-13 16:26:20 +0300

    🔁 Utility: Rename method to Random_Double_Normalized() & Add Clamp() API

--- Utility.h
+++ Utility.h
@@ -14,7 +14,7 @@ namespace Utility
 
 	/* Utility Functions */
 
-	inline double Random_Double()
+	inline double Random_Double_Normalized()
 	{
 		static std::uniform_real_distribution< double > distribution( 0.0, 1.0 );
 		static std::mt19937 generator;
@@ -24,5 +24,14 @@ namespace Utility
 	inline double DegreesToRadians( double degrees )
 	{
 		return degrees * PI / 180.0;
+	}
+
+	inline double Clamp( const double value, const double minimum, const double maximum )
+	{
+		return value < minimum
+					? minimum
+					: value > maximum
+						? maximum
+						: value;
 	}
 }
